bdbcd12f411c049ef21c62299f881b86c3b89035
Albert <Albertbekbulatov5@gmail.com>
2020-01-22 23:27:22 +0500

    домашка

---
+++ script.js
@@ -0,0 +1,34 @@
+var strName = prompt( ' введите имя ' );
+var Myname =(' Меня зовут ');
+var youranswer1 =( ' :Верный ответ "Обычно друзья меня зовут выпить"');
+console.log (Myname + strName + youranswer1 );
+//******************** */
+var strName = prompt('введите свой возраст');
+var myexample2 =('Мне -' );
+var youranswer2 =( ' :Верный ответ - Мне восемнадцать и 144 месяца');
+console.log(myexample2 + strName + youranswer2);
+//********************* */
+var aplus = +prompt('6 + 3 = ?');
+var myexample3 =('Пример-1; 6 + 3 = ' );
+var youranswer5 =( ' :Верный ответ- 9');
+console.log(myexample3 + aplus + youranswer5 );
+// **********************/
+var multiplication = +prompt('6 * 3 = ?');
+var myexample3 =('Пример-2; 6 * 3 =  ' );
+var youranswer5 =( ' :Верный ответ- 18');
+console.log(myexample3 + multiplication + youranswer5 );
+// **********************/
+var division = +prompt('6 / 3 = ?');
+var myexample3 =('Пример-3; 6 / 3 = ' );
+var youranswer5 =( ' :Верный ответ- 2');
+console.log(myexample3 + multiplication + youranswer5 );
+// **********************/
+var percent = +prompt('7 % 2 = ?');
+var myexample3 =('Пример-4; 7 % 2 = ' );
+var youranswer5 =( ' :Верный ответ- 1');
+console.log(myexample3 + percent + youranswer5 );
+// **********************/
+alert('Для проверки войдите в Console разработчика');
+
+
+
